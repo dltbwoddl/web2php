2efa542198c237b522639f5fae39bc0d6720a3ef
dltbwoddl <60048456+dltbwoddl@users.noreply.github.com>
2020-09-23 15:02:20 +0900

    Add files via upload

--- information.php
+++ information.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-<a href="/phppractice_1.html">web</a>
+<a href="/information.php">web</a>
     <ol>
         <li><a href="/information.php?id=css">css</a></li>
         <li><a href="/information.php?id=html">html</a></li>
@@ -14,10 +14,14 @@
         <li><a href="/information.php?id=mysql">mysql</a></li>
         <li><a href="/information.php?id=php">php</a></li>
     </ol>
-    <?php
+    <?php    
     $name = $_GET['id'];
-    echo $name;
-    echo file_get_contents("phppr/".$name.".txt");
+    if(isset($name)){
+        echo $name;
+        echo file_get_contents("phppr/".$name.".txt");
+    } else{
+        echo 'welcome';
+    }
     ?>
 </body>
 </html>
